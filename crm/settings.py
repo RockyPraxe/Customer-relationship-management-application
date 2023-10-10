6daa9384c8c20f11c8e46ca94f17b5e15bd18c89
RockyPraxe <filipio.svanda@gmail.com>
2023-10-10 23:58:58 +0000

    Fixing the allowed host

--- crm/settings.py
+++ crm/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rockypraxe-customerrela-h50y9zv919m.ws-eu105.gitpod.io', 'customer-r-m-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-rockypraxe-customerrela-h50y9zv919m.ws-eu105.gitpod.io', 'customer-r-m-app-8abc1380955d.herokuapp.com', 'localhost']
 
 
 # Application definition

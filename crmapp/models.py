aa601bdccf5193271b656158fd00a3777c5226b3
RockyPraxe <filipio.svanda@gmail.com>
2023-10-14 13:43:17 +0000

    Create models for product and order

--- crmapp/models.py
+++ crmapp/models.py
@@ -11,3 +11,27 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Nutritional supplement', 'Nutritional supplement'),
+        ('Training aids', 'Training aids'),
+            )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+            )
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)

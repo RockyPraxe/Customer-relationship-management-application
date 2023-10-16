2fb02abd6444c99cf3b4fa860ff26b535f92132e
RockyPraxe <filipio.svanda@gmail.com>
2023-10-16 10:30:13 +0000

    Add new model Tag

--- crmapp/models.py
+++ crmapp/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Nutritional supplement', 'Nutritional supplement'),
@@ -24,6 +31,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -35,7 +43,8 @@ class Order(models.Model):
         ('Out for Delivery', 'Out for Delivery'),
         ('Delivered', 'Delivered'),
             )
-
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 

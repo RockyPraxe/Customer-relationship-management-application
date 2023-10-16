cdac75dcfde245f58d2d1e805d1748154b175140
RockyPraxe <filipio.svanda@gmail.com>
2023-10-16 12:59:40 +0000

    rendering status data into dashboard template

--- crmapp/models.py
+++ crmapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -59,4 +60,4 @@ class Order(models.Model):
     note = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
-        return self.name
+        return self.product.name

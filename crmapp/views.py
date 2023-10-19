c241ef69e8af6ed03aa929116bf031d5c60e8062
RockyPraxe <filipio.svanda@gmail.com>
2023-10-19 22:46:28 +0000

    When new user is created is automaticly customer

--- crmapp/views.py
+++ crmapp/views.py
@@ -24,6 +24,7 @@ def registerPage(request):
 
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(user=user)
 
             messages.success(request, 'Account was created for' + username)
             return redirect('login')

f75add580ff5fcda16340eed953cf836135fffe5
RockyPraxe <filipio.svanda@gmail.com>
2023-10-23 22:45:38 +0000

    Add comments for apps and decorators.py

--- crmapp/apps.py
+++ crmapp/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
 
+# Define the configuration for the 'crmapp' Django app
+# Use the 'BigAutoField' for the default primary key
+# Define a method that is executed when the app is ready
+# Import and execute the signals defined in the 'crmapp.signals' module
+
+
 class CrmappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crmapp'
--- crmapp/decorators.py
+++ crmapp/decorators.py
@@ -2,6 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 
 
+# This function is a decorator that checks if a user is not authenticated.
+# If the user is authenticated, it redirects them to the home page; otherwise,
+# it allows them to access the specified view.
+
+
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -10,6 +15,12 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
 
     return wrapper_func
+
+
+# This function is a decorator that checks if the user belongs to one
+# of the specified roles (groups).
+# If the user's group is in the list of allowed roles, it allows access to the
+# view; otherwise, it denies access and displays a message.
 
 
 def allowed_users(allowed_roles=[]):
@@ -26,6 +37,12 @@ def allowed_users(allowed_roles=[]):
                 return HttpResponse('You are not authorized to view this page')
         return wrapper_func
     return decorator
+
+
+# This function is a decorator that restricts access to a view based on the
+# user's group.
+# If the user's group is customer, it redirects them to the user-page.
+# If the user's group is admin, it allows them to access the view.
 
 
 def admin_only(view_func):
